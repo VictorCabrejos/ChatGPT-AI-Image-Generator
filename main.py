1c7cbacb5c8adc2b13f165d5966a644c1329764d
vmc62 <thevictor23@gmail.com>
2023-12-08 10:29:50 -0500

    update main.oy to demonstrate varied text and image generation

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import ai_image_generator as img_gen
 
 def main():
     text_prompt = "Write a short story about a lost treasure."
+
+    image_prompt = "Create a hello kitty riding a bike in space"
+
     text_response = chatgpt.generate_text(text_prompt)
     print("ChatGPT Response:", text_response)
 
-    image_response = img_gen.generate_image("Illustration of a lost treasure")
+    image_response = img_gen.generate_image(image_prompt)
     print("AI Image Response:", image_response)
 
 if __name__ == "__main__":
