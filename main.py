b9c1aa9247763b3aadfe43a972f4ee28a3edf181
vmc62 <thevictor23@gmail.com>
2023-12-08 11:46:17 -0500

    new2-corrected commit

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import chatgpt_integration as chatgpt
 import ai_image_generator as img_gen
 
 def main():
-    text_prompt = "Write a poem about a mysterious forest."
+    # This is a new change to the main.py file
+    # Changed text prompt for a new story
+    text_prompt = "Write a dialogue between a wizard and a robot about magic vs science."
     text_response = chatgpt.generate_text(text_prompt)
     print("ChatGPT Response:", text_response)
 
-    image_prompt = "A futuristic cityscape at sunset"
+    # Changed image prompt for a new image generation task
+    image_prompt = "A surreal painting of a chessboard extending to the horizon under a sky full of stars"
     image_response = img_gen.generate_image(image_prompt)
     print("AI Image Response:", image_response)
 
